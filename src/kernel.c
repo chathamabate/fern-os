431db6fc8f383b2efc695d8811717c4c1f53a92d
chathamabate <chathamabate@gmail.com>
2024-10-12 21:45:37 -0600

    Working with the Global Descriptor Table

--- src/kernel.c
+++ src/kernel.c
@@ -23,10 +23,41 @@ void kernel_main(void) {
 
     read_gdtr(&v);
 
-    char buf[20];
-    str_fmt(buf, "Offset = 0x%X\n", (v.offset));
+    size_t len = (v.size_m_1 + 1) / 8;
+    seg_descriptor_t *tbl = (seg_descriptor_t *)(v.offset);
+
+    char buf[100];
+    str_fmt(buf, "Num Entries: %u\n", len);
     term_puts(buf);
 
-    str_fmt(buf, "Size   = 0x%X\n", (uint32_t)(v.size));
-    term_puts(buf);
+    // Looks like in this configuration, the entire memory space is read/write/executable!
+    // Looks like this is for paging setup at somepoint??
+    // Maybe I could change this at some point?
+    // Attempt changing this to some different value??
+
+    for (uint32_t i = 0; i < len; i++) {
+        seg_descriptor_t sd = tbl[i];
+        
+        uint32_t lim = sd_get_limit(sd);
+        uint32_t base = sd_get_base(sd);
+        uint32_t flags = sd_get_flags(sd);
+        uint32_t access_byte = sd_get_access_byte(sd);
+
+        str_fmt(buf, "\nEntry %u:\n", i);
+        term_puts(buf);
+
+        str_fmt(buf, "  Base  = %X\n", base);
+        term_puts(buf);
+
+        str_fmt(buf, "  Limit = %X\n", lim);
+        term_puts(buf);
+
+        str_fmt(buf, "  Flags = %X\n", flags);
+        term_puts(buf);
+
+        str_fmt(buf, "  AB    = %X\n", access_byte);
+        term_puts(buf);
+    }
+
+    return;
 }
--- src/mods/msys/include/msys/gdt.h
+++ src/mods/msys/include/msys/gdt.h
@@ -6,11 +6,31 @@
 #include <stdint.h>
 
 struct _gdtr_val_t {
-    uint16_t size;  // Comes first in memory.
+    uint16_t size_m_1; // size of the table (in bytes) - 1.
     uint32_t offset;
 } __attribute__ ((packed));
 
 typedef struct _gdtr_val_t gdtr_val_t;
+
+typedef uint64_t seg_descriptor_t;
+
+static inline uint32_t sd_get_limit(seg_descriptor_t sd) {
+    return (sd & 0xFFFF) | ((sd >> 32) & 0x000F0000);
+}
+
+static inline uint32_t sd_get_base(seg_descriptor_t sd) {
+    return ((sd >> 16) & 0xFFFF) | 
+        ((sd >> 16) & 0x00FF0000) |
+        ((sd >> 32) & 0xFF000000);
+}
+
+static inline uint8_t sd_get_flags(seg_descriptor_t sd) {
+    return (sd >> 52) & 0xF;
+}
+
+static inline uint8_t sd_get_access_byte(seg_descriptor_t sd) {
+    return (sd >> 40) & 0xFF;
+}
 
 // Stores contents of the gdtr into where dest points.
 // NOTE: This will write 
