0efe457f06cc809b2e303491bb205bec0dd99716
chathamabate <chathamabate@gmail.com>
2025-04-12 14:16:45 -0600

    Almost done with ID table draft

--- src/s_data/include/s_data/id_table.h
+++ src/s_data/include/s_data/id_table.h
@@ -6,6 +6,11 @@
 
 #include "s_mem/allocator.h"
 
+/**
+ * An ID is really just an index into the ID table.
+ *
+ * For this reason, it is a gaurantee that all ids are less than their table's capacity.
+ */
 typedef uint32_t id_t;
 
 typedef struct _id_table_entry_t {
@@ -63,4 +68,61 @@ typedef struct _id_table_t {
     id_table_entry_t *tbl;
 } id_table_t;
 
-// My brain is like totally fried tbh... Not sure what to do about this...
+/**
+ * Create a new id table with the given max capacity.
+ *
+ * Returns NULL on error.
+ */
+id_table_t *new_id_table(allocator_t *al, uint32_t mc);
+
+static inline id_table_t *new_da_id_table(uint32_t mc) {
+    return new_id_table(get_default_allocator(), mc);
+}
+
+/**
+ * NOTE: This does NOT clean up the data pointers.
+ *
+ * If some cleanup is needed there, that is your responsibility.
+ */
+void delete_id_table(id_table_t *idtb);
+
+static inline id_t idtb_null_id(id_table_t *idtb) { 
+    return idtb->max_cap;
+}
+
+/**
+ * This pops an ID from the free list and marks its cell as allocated.
+ */
+id_t idtb_pop_id(id_table_t *idtb);
+
+/**
+ * This pushes an ID's cell onto the free list.
+ *
+ * Does nothing if the cell is already free.
+ */
+void idtb_push_id(id_table_t *idtb, id_t id);
+
+/**
+ * Get the data pointer of a given Cell.
+ *
+ * Returns NULL if the id is invalid or if its cell isn't allocated.
+ */
+static inline void *idtb_get(id_table_t *idtb, id_t id) {
+    if (id >= idtb->cap || !(idtb->tbl[id].allocated)) {
+        return NULL;
+    }
+
+    return idtb->tbl[id].cell.data;
+}
+
+/**
+ * Set the data pointer of a given cell.
+ *
+ * Does nothing if the given id is invalid or if its cell isn't allocated.
+ */
+static inline void idtb_set(id_table_t *idtb, id_t id, void *dp) {
+    if (id < idtb->cap && idtb->tbl[id].allocated) {
+        idtb->tbl[id].cell.data = dp;
+    }
+}
+
--- src/s_data/src/id_table.c
+++ src/s_data/src/id_table.c
@@ -1,4 +1,66 @@
 
-int ttt(void) {
-    return 0;
+#include "s_data/id_table.h"
+#include "s_mem/allocator.h"
+
+#define IDTB_STARTING_CAP 0x10U
+
+id_table_t *new_id_table(allocator_t *al, uint32_t mc) {
+    if (mc == 0 || !al) {
+        return NULL;
+    }
+
+    id_table_t *idtb = al_malloc(al, sizeof(id_table_t));
+    if (!idtb) {
+        return NULL;
+    }
+
+    uint32_t starting_cap = IDTB_STARTING_CAP;
+    if (mc < starting_cap) {
+        starting_cap = mc;
+    }
+
+    id_table_entry_t *tbl = al_malloc(al, sizeof(id_table_entry_t) * starting_cap);
+    if (!tbl) {
+        al_free(al, idtb);
+        return NULL;
+    }
+
+    // Success!
+
+    idtb->al = al;
+    idtb->max_cap = mc;
+    idtb->cap = starting_cap;
+    idtb->fl_head = 0;
+
+    // Set up free list.
+    for (id_t id = 0; id < starting_cap; id++) {
+        tbl[id].allocated = false;
+        tbl[id].cell.next = id + 1;
+    }
+
+    // Last entry should point to NULL, it will be the end of the inital free list.
+    tbl[starting_cap - 1].cell.next = mc;
+
+    idtb->tbl = tbl;
+
+    return idtb;
+}
+
+void delete_id_table(id_table_t *idtb) {
+    if (!idtb) {
+        return;
+    }
+
+    allocator_t *al = idtb->al;
+
+    al_free(al, idtb->tbl);
+    al_free(al, idtb);
+}
+
+id_t idtb_pop_id(id_table_t *idtb) {
+
+}
+
+void idtb_push_id(id_table_t *idtb, id_t id) {
+    
 }
