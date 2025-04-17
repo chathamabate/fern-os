198cfbaa9e3d776471b9da130f1a83d1e1e51194
chathamabate <chathamabate@gmail.com>
2025-04-17 10:45:14 -0600

    Vector wait queue interface done

--- src/s_data/include/s_data/wait_queue.h
+++ src/s_data/include/s_data/wait_queue.h
@@ -65,6 +65,10 @@ static inline void wq_dump(wait_queue_t *wq, void (*pf)(const char *fmt, ...)) {
     }
 }
 
+/*
+ * Basic Wait Queue.
+ */
+
 /**
  * A basic_wait_queue_t is a pretty standard wait queue type.
  *
@@ -154,28 +158,123 @@ static inline void bwq_notify_all(basic_wait_queue_t *bwq) {
  */
 fernos_error_t bwq_pop(basic_wait_queue_t *bwq, void **item);
 
-
-
-/**
- * A sig_wait_queue_t is a queue which manages threads waiting on signals to arrive.
+/*
+ * Vector Ready Queue.
  */
-typedef struct _sig_wait_queue_t {
-    // TODO: Fill this in.
-    int nop;
-} sig_wait_queue_t;
 
 /**
- * Thread t should be woken up when any one of the signals marked in sig mask are received.
+ * How waiting items are stored.
  */
-//fernos_error_t swq_enqueue(sig_wait_queue_t *swq, thread_t *t, uint32_t sig_mask);
+typedef struct _vwq_wait_pair_t {
+    void *item;
+    uint32_t ready_mask;
+} vwq_wait_pair_t;
 
 /**
- * This should iterate over the threads in the queue. If t.mask | *sig_vec, then that thread
- * is woken up and given t.mask | *sig_vec, afterwards *sig_vec <= *sig_vec & ~t.mask.
+ * How ready items are stored.
+ */
+typedef struct _vwq_ready_pair_t {
+    void *item;
+    uint8_t ready_id;
+} vwq_ready_pair_t;
+
+/**
+ * The idea of a vector wait queue is that a set of 32 different events can affect the queue.
  *
- * This is done repeatedly until the signal vector is empty, or all threads have been searched.
- *
- * In one notify call, no two threads are ever woken up for the same signal!
+ * Each enqueued item is paired with a 32 bit vector which determines which of the events can
+ * "ready" the item.
  */
-void swq_notify(sig_wait_queue_t *swq, uint32_t *sig_vec);
+typedef struct _vector_wait_queue_t {
+    wait_queue_t super;
 
+    allocator_t *al;
+
+    /**
+     * The queue of waiting items. 
+     *
+     * list<vwq_wait_pair_t>
+     */
+    list_t *wait_q;
+
+    /**
+     * The ready queue.
+     *
+     * list<vwq_ready_pair_t>
+     */
+    list_t *ready_q;
+} vector_wait_queue_t;
+
+/**
+ * This ready ID is returned when there is nothing to pop from the ready queue. It represents
+ * an invalid ready ID.
+ */
+#define VWQ_NULL_READY_ID (32)
+
+/**
+ * Like the basic wait queue, the vector wait queue also has different notify modes.
+ */
+typedef uint32_t vwq_notify_mode_t;
+
+/**
+ * Notify the first waiting item which accepts the given id. (Remember by "first" we me the oldest
+ * item in the waiting queue, FIFO)
+ */
+#define VWQ_NOTIFY_FIRST (0)
+
+/**
+ * Notify ALL waiting items which accept the given id. (Loaded onto the ready queue in FIFO order)
+ */
+#define VWQ_NOTIFY_ALL   (1)
+
+/**
+ * Returns NULL in case of insufficient resources.
+ */
+vector_wait_queue_t *new_vector_wait_queue(allocator_t *al);
+
+static inline vector_wait_queue_t *new_da_vector_wait_queue(void) {
+    return new_vector_wait_queue(get_default_allocator());
+}
+
+/**
+ * Enqueue an item in the vector wait queue.
+ *
+ * The `ready_mask` is 32-bit value which describes which ids can ready this item when its in the
+ * wait queue.
+ *
+ * An error is returned if there aren't sufficient resources to enqueue item, or if the ready mask
+ * is 0.
+ */
+fernos_error_t vwq_enqueue(vector_wait_queue_t *vwq, void *item, uint32_t ready_mask);
+
+/**
+ * Notify waiting items depending on the given ready_id.
+ * ready_id MUST be in the range 0-31. Only threads which have the bit (1 << ready_id) set in
+ * their ready mask are able to be readied. Which ones are readied depends on the mode set.
+ *
+ * An error is returned if we don't have sufficient resources. In this case the notification 
+ * procedure will stop. All items which were readied will remain in the ready queue. All items
+ * which failed to be readied will remain in the waiting queue.
+ *
+ * An error is also returned if ready_id >= 32.
+ */
+fernos_error_t vwq_notify(vector_wait_queue_t *vwq, uint8_t ready_id, vwq_notify_mode_t mode);
+
+static inline fernos_error_t vwq_notify_first(vector_wait_queue_t *vwq, uint8_t ready_id) {
+    return vwq_notify(vwq, ready_id, VWQ_NOTIFY_FIRST);
+}
+
+static inline fernos_error_t vwq_notify_all(vector_wait_queue_t *vwq, uint8_t ready_id) {
+    return vwq_notify(vwq, ready_id, VWQ_NOTIFY_ALL);
+}
+
+/**
+ * Pop an item off the ready queue.
+ *
+ * Returns FOS_EMPTY if there are no ready items. In this case, NULL should be written to item 
+ * (if given), and VWQ_NULL_READY_ID should be written to ready_id (if given).
+ *
+ * Returns FOS_SUCCESS if there is at least one item in the ready queue. In this case, the
+ * item is popped from the ready queue. If item is given, the popped item is written at *item.
+ * If ready_id is given, the ready_id which readied the popped item will be written at *ready_id.
+ */
+fernos_error_t vwq_pop(vector_wait_queue_t *vwq, void **item, uint8_t *ready_id);
