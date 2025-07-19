245654aecc6ec78d0ca89a00d33f456d47705c4b
chathamabate <chathamabate@gmail.com>
2025-07-18 21:00:57 -0600

    Time for testing, but my head hurts

--- src/s_block_device/src/cached_block_device.c
+++ src/s_block_device/src/cached_block_device.c
@@ -60,7 +60,7 @@ block_device_t *new_cached_block_device(allocator_t *al, block_device_t *bd, siz
                 if (!(c[i].sector)) {
                     cache_alloc_fail = true;
                 } else {
-                    c[i].next_free = i + 1;
+                    c[i].i.next_free = i + 1;
                 }
             }
         }
@@ -223,6 +223,55 @@ static fernos_error_t cbd_write(block_device_t *bd, size_t sector_ind, size_t nu
     return FOS_SUCCESS;
 }
 
+/**
+ * No parameter checks, this is an internal helper which writes out the cache entry index of a given
+ * sector.
+ *
+ * If the sector is not in the cache, it will be read into the cache. 
+ * (Potentially overwriting some other cache entry)
+ */
+static fernos_error_t cbd_get_cache_entry_ind(cached_block_device_t *cbd, size_t sector_ind, size_t *cache_entry_ind_p) {
+    fernos_error_t err;
+
+    size_t *ind_p = mp_get(cbd->sector_map, &sector_ind);
+    size_t ind;
+
+    if (!ind_p) {
+        // Cache Miss!  (Let's load a sector into the cache!)
+
+        if (cbd->next_free != cbd->cache_cap) {
+            // Cache is not full.
+            
+            ind = cbd->next_free;
+
+            // Advance free list.
+            cbd->next_free = cbd->cache[cbd->next_free].i.next_free;
+        } else {
+            // Cache is full. (Remove a used sector.
+            
+            ind = next_rand(&(cbd->r)) % cbd->cache_cap;
+        }
+
+        cbd->cache[ind].i.sector_ind = sector_ind;
+
+        err = mp_put(cbd->sector_map, &sector_ind, &ind);
+        if (err != FOS_SUCCESS) {
+            return err; 
+        }
+
+        err = bd_read(cbd->wrapped_bd, sector_ind, 1, cbd->cache[ind].sector); 
+        if (err != FOS_SUCCESS) {
+            return err; 
+        }
+    } else {
+        ind = *ind_p;
+    }
+
+    *cache_entry_ind_p = ind;
+
+    return FOS_SUCCESS;
+}
+
 static fernos_error_t cbd_read_piece(block_device_t *bd, size_t sector_ind, size_t offset, size_t len, void *dest) {
     cached_block_device_t *cbd = (cached_block_device_t *)bd;
 
@@ -239,38 +288,10 @@ static fernos_error_t cbd_read_piece(block_device_t *bd, size_t sector_ind, size
 
     fernos_error_t err;
 
-    size_t *cache_entry_ind_p = mp_get(cbd->sector_map, &sector_ind); 
     size_t cache_entry_ind;
-
-    if (!cache_entry_ind_p) {
-        // Cache Miss!  (Let's load a sector into the cache!)
-
-        if (cbd->next_free != cbd->cache_cap) {
-            // Cache is not full.
-            
-            cache_entry_ind = cbd->next_free;
-
-            // Advance free list.
-            cbd->next_free = cbd->cache[cbd->next_free].i.next_free;
-        } else {
-            // Cache is full. (Remove a used sector.
-            
-            cache_entry_ind = next_rand(&(cbd->r)) % cbd->cache_cap;
-        }
-
-        cbd->cache[cache_entry_ind].i.sector_ind = sector_ind;
-
-        err = mp_put(cbd->sector_map, &sector_ind, &cache_entry_ind);
-        if (err != FOS_SUCCESS) {
-            return err; 
-        }
-
-        err = bd_read(cbd->wrapped_bd, sector_ind, 1, cbd->cache[cache_entry_ind].sector); 
-        if (err != FOS_SUCCESS) {
-            return err; 
-        }
-    } else {
-        cache_entry_ind = *cache_entry_ind_p;
+    err = cbd_get_cache_entry_ind(cbd, sector_ind, &cache_entry_ind);
+    if (err != FOS_SUCCESS) {
+        return err;
     }
 
     // Copy from cache into the dest buffer.
@@ -282,14 +303,97 @@ static fernos_error_t cbd_read_piece(block_device_t *bd, size_t sector_ind, size
 }
 
 static fernos_error_t cbd_write_piece(block_device_t *bd, size_t sector_ind, size_t offset, size_t len, const void *src) {
+    cached_block_device_t *cbd = (cached_block_device_t *)bd;
+
+    if (!src) {
+        return FOS_BAD_ARGS;
+    }
+
+    const size_t ns = bd_num_sectors(cbd->wrapped_bd);
+    const size_t ss = bd_sector_size(cbd->wrapped_bd);
+
+    if (sector_ind >= ns || offset >= ss || offset + len > ss || offset + len < offset) {
+        return FOS_INVALID_RANGE;
+    }
+
+    fernos_error_t err;
+
+    size_t cache_entry_ind;
+    err = cbd_get_cache_entry_ind(cbd, sector_ind, &cache_entry_ind);
+    if (err != FOS_SUCCESS) {
+        return err;
+    }
+
+    // Copy from cache into the dest buffer.
+
+    uint8_t *cache_sector = cbd->cache[cache_entry_ind].sector;
+    mem_cpy(cache_sector + offset, src, len);
+
     return FOS_SUCCESS;
 }
 
 static fernos_error_t cbd_flush(block_device_t *bd) {
+    cached_block_device_t *cbd = (cached_block_device_t *)bd;
+
+    // First thing we do is write out all the cached sectors.
+
+    fernos_error_t err;
+
+    const size_t *sector_ind;
+    size_t *cache_entry_ind;
+
+    mp_reset_iter(cbd->sector_map);
+    for (err = mp_get_iter(cbd->sector_map, (const void **)&sector_ind, (void **)&cache_entry_ind);
+            err == FOS_SUCCESS; err = mp_next_iter(cbd->sector_map, (const void **)&sector_ind, (void **)&cache_entry_ind)) {
+        err = bd_write(cbd->wrapped_bd, *sector_ind, 1, 
+                cbd->cache[*cache_entry_ind].sector);  
+
+        if (err != FOS_SUCCESS) {
+            return err;
+        }
+    }
+
+    if (err != FOS_EMPTY) {
+        return err;
+    }
+
+    // This could be done better if I implemented a mp_clear function.
+    //
+    // The idea is if we just empty the sector map, the cache appears empty.
+    // I just delete and recreate the map here.
+
+    delete_map(cbd->sector_map);    
+
+    *(map_t **)&(cbd->sector_map) = new_chained_hash_map(cbd->al, 
+            sizeof(size_t), sizeof(size_t), 3, 
+            size_t_eq_f, size_t_hash_f);
+
+    if (!(cbd->sector_map)) {
+        return FOS_NO_MEM;
+    }
+
+    for (size_t i = 0; i < cbd->cache_cap; i++) {
+        cbd->cache[i].i.next_free = i + 1;
+    }
+
+    cbd->next_free = 0;
+
     return FOS_SUCCESS;
 }
 
 static void delete_cached_block_device(block_device_t *bd) {
-    return FOS_SUCCESS;
+    cached_block_device_t *cbd = (cached_block_device_t *)bd;
+
+    delete_map(cbd->sector_map);
+
+    for (size_t i = 0; i < cbd->cache_cap; i++) {
+        al_free(cbd->al, cbd->cache[i].sector);
+    }
+
+    al_free(cbd->al, cbd->cache);
+
+    delete_block_device(cbd->wrapped_bd);
+
+    al_free(cbd->al, cbd);
 }
 
