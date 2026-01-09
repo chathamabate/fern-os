dc8ca2c59aa3f930539816d536376ebee58d7fc0
chathamabate <chathamabate@gmail.com>
2026-01-08 20:03:04 -0500

    Graphics discalimer

--- src/u_startup/src/main.c
+++ src/u_startup/src/main.c
@@ -2,7 +2,15 @@
 #include "u_startup/main.h"
 
 proc_exit_status_t user_main(void) {
-    // User code here.
+
+    /*
+     * User Code.
+     *
+     * NOTE: This MR stips view of the VGA BIOS terminal. However, it does not give the user
+     * access to the new graphics display buffer.
+     *
+     * The user has no ability to render anything to the screen at this commit.
+     */
 
     return PROC_ES_SUCCESS;
 }
