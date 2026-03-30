6af2f103ef11de72fc2877b9e60dda462fc9a59c
chathamabate <chathamabate@gmail.com>
2026-03-29 20:00:48 -0700

    Slow testing progress

--- src/u_startup/src/syscall_shm.c
+++ src/u_startup/src/syscall_shm.c
@@ -6,11 +6,20 @@ fernos_error_t sc_shm_new_semaphore(sem_id_t *sem, uint32_t num_passes) {
     return sc_plg_cmd(PLG_SHARED_MEM_ID, PLG_SHM_PCID_NEW_SEM, (uint32_t)sem, num_passes, 0, 0);
 }
 
+/*
+ * I add in memory barriers here. They aren't really necessary given we are using
+ * a single core, but whatever!
+ */
+
 fernos_error_t sc_shm_sem_dec(sem_id_t sem) {
-    return sc_plg_cmd(PLG_SHARED_MEM_ID, PLG_SHM_PCID_SEM_DEC, (uint32_t)sem, 0, 0, 0);
+    fernos_error_t err = sc_plg_cmd(PLG_SHARED_MEM_ID, PLG_SHM_PCID_SEM_DEC, (uint32_t)sem, 0, 0, 0);
+    __sync_synchronize();
+
+    return err; 
 }
 
 void sc_shm_sem_inc(sem_id_t sem) {
+    __sync_synchronize();
     (void)sc_plg_cmd(PLG_SHARED_MEM_ID, PLG_SHM_PCID_SEM_INC, (uint32_t)sem, 0, 0, 0);
 }
 
--- src/u_startup/test/syscall_shm.c
+++ src/u_startup/test/syscall_shm.c
@@ -8,12 +8,20 @@
 
 #include "s_util/test.h"
 
-static bool test1(void) {
+static bool test_sem_new_and_close(void) {
+    sem_id_t sem;
+
+    TEST_EQUAL_HEX(FOS_E_BAD_ARGS, sc_shm_new_semaphore(NULL, 10));
+    TEST_EQUAL_HEX(FOS_E_BAD_ARGS, sc_shm_new_semaphore(&sem, 0));
+
+    TEST_SUCCESS(sc_shm_new_semaphore(&sem, 2));
+    sc_shm_close_semaphore(sem);
+
     TEST_SUCCEED();
 }
 
 bool test_syscall_shm_sem(void) {
     BEGIN_SUITE("Shared Memory: Semaphores");
-    RUN_TEST(test1);
+    RUN_TEST(test_sem_new_and_close);
     return END_SUITE();
 }
