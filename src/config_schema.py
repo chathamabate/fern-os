a65421100ca97ed5082d23a912b184a246d65d25
chathamabate <chathamabate@gmail.com>
2026-07-25 20:12:54 -0700

    Starting to make sense here

--- src/config_schema.py
+++ src/config_schema.py
@@ -4,9 +4,93 @@ from fernconf.FCTranslator import *
 from fernconf.FCSchema import *
 from fernconf.FCTooling import *
 
-class FernRange(FCSchema):
-    def __init__(self):
-        pass
+FOS_UINT = FCS_INT.with_extra_checks(
+    non_neg=lambda fcv: Ok(None) if  cast(int, fcv) >= 0 else Err("value must be non-negative")
+)
+
+FOS_UINT32 = FOS_UINT.with_extra_checks(
+    u32=lambda fcv: Ok(None) if cast(int, fcv) < 0x1_0000_0000 else Err("value too large for 32-bits")
+)
+
+def fern_os_range32_not_empty(fcv: FCValue) -> Result[None, str]:
+    dv = cast(dict[str, FCValue], fcv)
+
+    start = cast(int, dv["START"])
+    end_inc = cast(int, dv["END_INC"])
+
+    if end_inc < start:
+        return Err(f"empty range provided: [{start}, {end_inc}]")
+
+    return Ok(None)
+
+def fern_os_range32_4k_aligned(fcv: FCValue) -> Result[None, str]:
+    dv = cast(dict[str, FCValue], fcv)
+
+    start = cast(int, dv["START"])
+    end_inc = cast(int, dv["END_INC"])
+
+    if start & (0x1000 - 1) != 0:
+        return Err(f"range start is not 4k aligned")
+
+    if (end_inc + 1) & (0x1000 - 1) != 0:
+        return Err(f"range exclusive end is not 4k aligned")
+
+    return Ok(None)
+
+def fern_os_range32_exclusive_end_translate(prefix: str, value: FCValue, translator: FCTranslator) -> list[str]:
+    dv = cast(dict[str, FCValue], value)
+    end = cast(int, dv["END_INC"]) + 1
+
+    # It is possible the value of `end` is 0x1_0000_0000
+    return translator.definition(prefix + "_END", end)
+
+FOS_RANGE32 = FCSchemaStruct([
+    ("START", FOS_UINT32),
+    ("END_INC", FOS_UINT32),
+]).with_extra_checks(
+        non_empty=fern_os_range32_not_empty, align_4k=fern_os_range32_4k_aligned
+).with_translates(fos_os_range32_exclusive_end_translate)
+
+def fern_os_mem_ranges_no_overlap(fcv: FCValue) -> Result[None, str]:
+    dv = cast(dict[str, FCValue], value)
+
+    return Ok(None)
+
+FOS_MEM_RANGES = FCSchemaStruct([
+    ("KERNEL", FOS_RANGE32.with_comment([
+        "This is where the kernel elf is loaded into.",
+        "",
+        "NOTE: The linker script exposes the finer grained areas inside this range."
+    ])),
+    ("APP", FOS_RANGE32.with_comment([
+        "Where user application elf files are loaded."
+    ])),
+    ("APP_ARGS", FOS_RANGE32.with_comment([
+        "Where user application arguments are loaded."
+    ])) ,
+    ("FREE", FOS_RANGE32.with_comment([
+        "The dynamic area region for user processes.",
+        "i.e. where you can request_mem from."
+    ])),
+    ("SHARED", FOS_RANGE32.with_comment([
+        "Where shared pages area placed."
+    ])),
+    ("STACK", FOS_RANGE32.with_comment([
+        "Where kernel and user thread stacks live."
+    ]))
+])
+
+
+
+FOS_MEM_LAYOUT = FCSchemaStruct([
+    ("FULL_AREA", FOS_RANGE32.with_comment([
+        "Virtual AND Physical memory area owned entirely by FernOS.",
+        "",
+        "NOTE: Other areas may still be used depending on where registers live and how the",
+        "bootloader sets things up."
+    ])),
+    ("RANGES", FOS_MEM_RANGES)
+])
 
 
 fern_os_config_schema = FCSchemaStruct([
--- src/u_startup/src/main.c
+++ src/u_startup/src/main.c
@@ -1,10 +1,69 @@
 
 #include "u_startup/main.h"
 
+#include "u_startup/syscall_gfx.h"
+#include "u_startup/syscall.h"
+#include "u_startup/test/syscall_gfx.h"
+#include "s_mem/simple_heap.h"
+#include "s_mem/test/simple_heap.h"
+#include "u_startup/test/syscall.h"
+#include "u_startup/test/syscall_fs.h"
+#include "s_data/test/binary_search_tree.h"
+
 proc_exit_status_t user_main(void) {
     /*
      * User Code Here.
      */
+
+    // Ok, potential directions:
+    //      * Better userspace terminal?
+    //      * Userspace graphics window infra?
+    //      * Reworked testing? (Could be fun and useful)
+    //      I mean some sort of way of running many tests in a row?
+    //      IDK, this is kinda difficult in this env tbh?
+    //      Certain tests can only be run at certain times anyway?
+    //      I guess it'd be cool to have a better place for some of this shit?
+    //      IDK, I feel like configurations aren't that bad at the moment?
+    //      IDK, I mean, it's not great...
+    //      I guess what I am trying to say is that it would be cool if we could store multiple
+    //      multiple configurations in different places??? Right... we can keep constraints in C
+    //      file right???
+    //      I mean, it doesn't really need to be that complex imo.
+    //      We can still leave errors inside the C code imo.
+    //
+    //      Configs can live as JSON
+    //      JSON can be converted -> C header, LD Header, and ASM?
+    //      Not the end of the world?
+    //      I mean, would this really be worth it??
+    //      I think so..... I mean, most of all it would be fun!
+    //      We may want to consider doing ISO Loading first though???
+    //      Ultimately, we are trying to produce a single image right??
+    //      Well, that's not totally true, we may want to produce many different things...
+    //      IDK, I don't really event want to do anything right now, plus it's raining.
+    //      Should we build a fernconf tool???
+    //      I mean, that could be kinda cool tbh... Why even put it in this repo?
+    //      Ok, but won't constraints be specified in python???
+    //      What if we want quick changes and shit??
+    //      Git submodules aren't so bad IMO? But I think we'd want pip for that tbh.
+    //      Yes, this is also true...
+    //
+
+    handle_t h;
+    sc_gfx_new_terminal(&h, NULL);
+    sc_set_out_handle(h);
+
+    test_syscall();
+
+    // I think this guy requires a heap?
+    // I mean, we could make this work a little bit?
+    // could be a fun 
+    // Maybe the possibility of running many tests?
+    // Like maybe the userspace tests could be better?
+    // IDK, just throwing that out there?
+    test_syscall_fs();
+
+    test_syscall_gfx();
+    while (1);
 
     return PROC_ES_SUCCESS;
 }
