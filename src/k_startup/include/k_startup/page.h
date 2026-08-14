907e09fc55fcd94eacb2c85967ec928b20096a99
Chatham Abate <chathamabate@gmail.com>
2026-08-13 22:15:16 -0700

    Better physical free list detection! (#34)

--- src/k_startup/include/k_startup/page.h
+++ src/k_startup/include/k_startup/page.h
@@ -12,9 +12,12 @@
  * Here are all the section layouts as defined in the linker script.
  *
  * NOTE: end is EXCLUSIVE!
- *
- * The ends is os_defs are INCLUSIVE!
  */
+
+extern uint8_t _static_area_start[];
+
+extern uint8_t _multiboot_start[];
+extern uint8_t _multiboot_end[];
 
 extern uint8_t _sys_tables_start[];
 extern uint8_t _sys_tables_end[];
